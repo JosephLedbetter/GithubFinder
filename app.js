a7dbf8f8378368d1fff9ad5447c6a775594a4176
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2020-04-30 14:20:50 -0400

    finished the repos pull and connection to the user profile in github

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ searchUser.addEventListener('keyup', (e) => {
         // show profile
     } else {
         ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
     }
 })
     } else {
--- github.js
+++ github.js
@@ -13,7 +13,7 @@ class Github {
         const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret${this.client_secret}`);
         
         const profile = await profileResponse.json();
-        const profile = await repoResponse.json();
+        const repos = await repoResponse.json();
 
         return {
             profile,
